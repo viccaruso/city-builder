5671852e4256a74b9df5d37f44bd39857bde0e51
Victor Caruso <vic.caruso@gmail.com>
2021-12-03 12:02:27 -0800

    been working on many things

--- app.js
+++ app.js
@@ -1,8 +1,60 @@
-// import functions and grab DOM elements
+// Import functions from utils.js 
+import { displayStats } from './utils.js';
 
-// let state
+// Grab DOM elements and store in variable
+const buildingImgEl = document.querySelector('#buildings-image');
+const waterfrontImgEl = document.querySelector('#waterfront-image');
+const parkImgEl = document.querySelector('#park-image');
+const buildingsDropdownEl = document.querySelector('#buildings-dropdown');
+const waterfrontDropdownEl = document.querySelector('#waterfront-dropdown');
+const parkDropdownEl = document.querySelector('#park-dropdown');
+const sloganTextFieldEl = document.querySelector('#slogan-field');
+const addSloganButton = document.querySelector('#slogan-add-button');
+const sloganDisplayEl = document.querySelector('slogan-display');
 
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+// Set initial state values
+let buildingChanges = 0;
+let waterfrontChanges = 0;
+let parkChanges = 0;
+const sloganArray = [];
+
+// Set up event listeners 
+buildingsDropdownEl.addEventListener('change', () => {
+    // Update state
+    buildingChanges++;
+
+    console.log(buildingsDropdownEl.value);
+
+    // Reset DOM and display state changes
+    clearDOM();
+    displayStats(buildingChanges, waterfrontChanges, parkChanges);
+    
+});
+
+waterfrontDropdownEl.addEventListener('change', () => {
+    waterfrontChanges++;
+    console.log('changed');
+});
+
+parkDropdownEl.addEventListener('change', () => {
+    parkChanges++;
+    console.log('changed');
+});
+
+addSloganButton.addEventListener('click', () => {
+    sloganArray.push(sloganTextFieldEl.value);
+    sloganTextFieldEl.value = ''; 
+    clearDOM();
+    displaySlogan(sloganArray);
+});
+
+// Helper functions
+function clearDOM() {
+    console.log('clearing dom');
+}
+
+function displaySlogan(array) {
+    console.log('displaying slogan');
+    console.log(array);
+}
+
---
+++ utils.js
@@ -0,0 +1,3 @@
+export function displayStats(count1, count2, count3) {
+    console.log(count1, count2, count3);
+}
