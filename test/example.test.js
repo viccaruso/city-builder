d33ecaf310eec9987cbf7103e22dd3622892ee37
Victor Caruso <vic.caruso@gmail.com>
2021-12-03 12:10:57 -0800

    test working for displayStats

--- test/example.test.js
+++ test/example.test.js
@@ -1,5 +1,6 @@
 // IMPORT MODULES under test here:
 // import { example } from '../example.js';
+import { displayStats } from '../utils.js';
 
 const test = QUnit.test;
 
@@ -11,6 +12,20 @@ test('time to test a function', (expect) => {
     //Act 
     // Call the function you're testing and set the result to a const
     const actual = true;
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
+test('displayStats should return a string with the correct count of changes made', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = 'You changed the buildings 2 times, the waterfront 6 times, and the park 1 time. And everybody loves your city\'s slogans:';
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = displayStats(2, 6, 1);
 
     //Expect
     // Make assertions about what is expected versus the actual result
